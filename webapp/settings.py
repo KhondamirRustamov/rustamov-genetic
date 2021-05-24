51faf2f980581d607b73f1ba73b643adf04ffe4c
Khondamir Rustamov <69417652+KhondamirRustamov@users.noreply.github.com>
2021-05-24 21:42:43 +0500

    Add files via upload

--- webapp/settings.py
+++ webapp/settings.py
@@ -15,18 +15,17 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-55$1=ok0y2uexvw(io@yr_'
+SECRET_KEY = 'django-insecure-55$1=ok0y2uex%4up&9#*z#@#(y22i^q_h%)t5^hovw(io@yr_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://rustamov-genetic.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -122,13 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
